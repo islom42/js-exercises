6f6513b9a26f22c04d14c06b9ca0d95a1cfd3877
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-05-07 13:21:38 +0300

    Update with exercise

---
+++ js-exercises/06 makes10.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/* makes10
+Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true
+*/
+
+const makes10 = (a, b) => {
+  return a === 10 || b === 10 || a + b === 10;
+};
+console.log(makes10(9, 10));
+console.log(makes10(9, 9));
+console.log(makes10(1, 9));
